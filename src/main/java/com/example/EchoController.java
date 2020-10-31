8bd094287951452ab3b9b32b0a52610423805c50
haappyy <57199984+haappyy@users.noreply.github.com>
2020-10-31 09:37:11 +0100

    Add missing comment to echo endpoint

--- src/main/java/com/example/EchoController.java
+++ src/main/java/com/example/EchoController.java
@@ -8,6 +8,11 @@ import io.micronaut.http.annotation.Post;
 @Controller("/")
 public class EchoController {
 
+    /**
+     * Returns the given request body.
+     * @param text
+     * @return the same as the text parameter
+     */
     @Post(uris = {"/echo"},produces = MediaType.TEXT_PLAIN)
     public String echo(@Body String text) {
         return text;
