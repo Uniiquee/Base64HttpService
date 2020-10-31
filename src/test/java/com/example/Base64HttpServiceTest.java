3daf8d4ef4ed8434eadd6c124508a9951f758a68
haappyy <57199984+haappyy@users.noreply.github.com>
2020-10-31 09:38:24 +0100

    Add a test for the echo controller

--- src/test/java/com/example/Base64HttpServiceTest.java
+++ src/test/java/com/example/Base64HttpServiceTest.java
@@ -1,14 +1,28 @@
 package com.example;
 
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import javax.inject.Inject;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @MicronautTest
 public class Base64HttpServiceTest {
+
+    @Inject
+    EmbeddedServer server;
+
+    @Inject
+    @Client("/")
+    HttpClient client;
 
     @Inject
     EmbeddedApplication application;
@@ -16,6 +30,12 @@ public class Base64HttpServiceTest {
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testEcho(){
+        String response = client.toBlocking().retrieve(HttpRequest.POST("/echo","test"));
+        assertEquals("test", response); //)
     }
 
 }
